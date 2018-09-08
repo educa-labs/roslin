1a044dd1dbf9747b8c29c1a7653fee4238841229
ironcadiz <afcadiz@uc.cl>
2018-09-08 14:27:03 -0300

    added pipeline and changed post to get

--- app.py
+++ app.py
@@ -5,13 +5,35 @@ from flask_cors import CORS, cross_origin
 # System libs
 import json
 # Other libs
+from sklearn.pipeline import Pipeline
+
+from transformers.trees import BallTreePredictor
+from transformers.json_transformers import JsonToTagsTransform
+from transformers.embedders import LdaTransformer
+
+#Constants
+DATA_PATH= "data.json"
+
+
+# Data and initial pipe training
+pipe_components =[("json",JsonToTagsTransform()),("embedder",LdaTransformer()),("tree",BallTreePredictor())] 
+pipe = Pipeline(pipe_components)
+with open(DATA_PATH) as file:
+    data =  json.load(file)
+pipe.fit(data)
+
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/v1/recommendations", methods = ["POST"])
+@app.route("/api/v1/recommendations", methods = ["GET"])
 def recommendations():
-    params = request.get_json()
-    print(params)
+    params = request.args.get('project_meta_ids')
+    try:
+        query = [int(x) for x in params[1:-1].split(",")]
+    except TypeError:
+        return Response(json.dumps({"error": "bad params"}, status=400))
+
+    print(query)
 
     # Dummmy response
     dummy = json.loads("""{
