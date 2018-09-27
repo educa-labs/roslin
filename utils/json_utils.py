fa442dfe10eeb7d591f832890b7e70a82a7751d0
ironcadiz <afcadiz@uc.cl>
2018-09-27 14:52:11 -0300

    added quick_codes support

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -12,15 +12,15 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 #returns documents with id in ids
-def get_by_id(data,ids):
+def get_by_id(data,ids,id_key='quick_code'):
     if type(ids) == list:
-        return list(filter(lambda x: x['id'] in ids, data))
+        return list(filter(lambda x: x[id_key] in ids, data))
 
-def map_to_id(data):
-    return [int(document['id']) for document in data]
+def map_to_id(data,id_key='quick_code'):
+    return [document[id_key] for document in data]
 
-def map_from_id(data):
-    return { document['id']:i for i,document in enumerate(data)}
+def map_from_id(data,id_key='quick_code'):
+    return { document[id_key]:i for i,document in enumerate(data)}
 
 
 def get_by_id_map(data,ids,map):
